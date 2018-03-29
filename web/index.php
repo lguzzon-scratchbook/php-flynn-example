a6017008d2f9515c60a469274ae976f597544b44
Luca Guzzon <luca.guzzon@gmail.com>
2018-03-29 13:12:57 +0000

    Timezone fixed

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ date_default_timezone_set();
 $app = new \Slim\Slim();
 
 $app->get('/', function () {
-    echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname() . " at date [" . date("D M d, Y G:i a") . "]\n";
+    echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname() . " at date [" . date("D M d, Y G:i:s a") . "]\n";
 });
 
 $app->run();
