df41076c3007e410e98871e301600305c793f04c
Luca Guzzon <luca.guzzon@gmail.com>
2018-03-29 12:45:50 +0000

    String concat fix

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ require('../vendor/autoload.php');
 $app = new \Slim\Slim();
 
 $app->get('/', function () {
-    echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname() . " at date [" . date("D M d, Y G:i a") "]\n";
+    echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname() . " at date [" . date("D M d, Y G:i a") . "]\n";
 });
 
 $app->run();
