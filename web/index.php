e9f4af97fd2ada6f52b559326d6910b1785bdf7b
Luca Guzzon <luca.guzzon@gmail.com>
2018-03-29 13:06:20 +0000

    Timezone fixed

--- web/index.php
+++ web/index.php
@@ -2,10 +2,11 @@
 
 require('../vendor/autoload.php');
 
+date_default_timezone_set();
+
 $app = new \Slim\Slim();
 
 $app->get('/', function () {
-    date_default_timezone_set();
     echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname() . " at date [" . date("D M d, Y G:i a") . "]\n";
 });
 
